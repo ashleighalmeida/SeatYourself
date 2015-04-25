6e165591781b739345430144ca59a5c86797101e
Ashleigh <ashleighalmeida@gmail.com>
2015-04-25 17:28:36 -0400

    User model /  signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'users/create'
+
   get 'restaurants' => 'restaurants#index'
   get 'restaurants/:id' => 'restaurants#show', as: 'restaurant'
+
+  resources :rest
+  resources :users, only: [:new, :create]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425205709) do
+ActiveRecord::Schema.define(version: 20150425211928) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20150425205709) do
     t.string   "imgurl"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
